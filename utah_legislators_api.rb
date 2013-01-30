2e36a5fdd0fd7983ff2f77b2ff3663c5e13b9368
Scott Riding <sriding@targetpointconsulting.com>
2013-01-30 01:51:16 -0500

    Revert "Added JSONP support."
    This reverts commit 5904d273ac9c5cf54ff76fc04f711c560774fe00.

--- utah_legislators_api.rb
+++ utah_legislators_api.rb
@@ -1,5 +1,4 @@
 require 'sinatra/base'
-require 'sinatra/jsonp'
 require './settings'
 require_relative 'models/bouncer'
 require_relative 'models/utah_address'
@@ -16,28 +15,23 @@ class UtahLegislatorsAPI < Sinatra::Base
   ##### Basic API #####
   
   get '/house/:district' do
-    rep = UtahLegislature::Representative.by_district(params[:district])
-    JSONP rep.to_json
+    UtahLegislature::Representative.by_district(params[:district]).to_json
   end
   
   get %r{/house(/)?} do
-    reps = UtahLegislature::Representative.all_districts
-    JSONP reps.to_json
+    UtahLegislature::Representative.all_districts.to_json
   end
   
   get '/senate/:district' do
-    senator = UtahLegislature::Senator.by_district(params[:district])
-    JSONP senator.to_json
+    UtahLegislature::Senator.by_district(params[:district]).to_json
   end
   
   get %r{/senate(/)?} do
-    senators = UtahLegislature::Senator.all_districts
-    JSONP senators.to_json
+    UtahLegislature::Senator.all_districts.to_json
   end
   
   get '/representation' do
-    reps = UtahLegislature::Representation.find(params[:address], params[:area])
-    JSONP reps.to_json
+    UtahLegislature::Representation.find(params[:address], params[:area]).to_json
   end
   
   ##### Exceptional Issues #####
